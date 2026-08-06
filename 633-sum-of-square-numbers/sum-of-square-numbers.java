14741171f1cc84da1e6b5698ba8799ff9c5da37c
tomardevsya2006-beep <tomardevsya2006@gmail.com>
2026-08-07 01:23:02 +0530

    Time: 4 ms (82.46%) | Memory: 42.2 MB (32.38%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long s = 0;
+        long l = (long) Math.sqrt(c);
+
+        while (s <= l) {
+            long sum = s * s + l * l;
+            
+            if (sum == c) {
+                return true;
+            } else if (sum < c) {
+                s++;
+            } else {
+                l--;
+            }
+        }
+
+        return false;
+    }
+}
