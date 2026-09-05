4a0561dbaa69b4b4c118036a81745d03d2ec20a5
DEVSYA TOMAR <tomardevsya2006@gmail.com>
2026-09-05 15:03:25 +0530

    Time: 2 ms (89.03%) | Memory: 47.6 MB (91.30%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+     int n=nums.length-1;
+     int max=0;
+     for(int i=0;i<=n;i++){
+        if(max<i) return false;
+        max=Math.max(max,i+nums[i]);
+     }
+    return true;
+    }
+}
